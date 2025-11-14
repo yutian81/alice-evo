2a1cc46c56cf25dd2a2f651a8864756f67c12d95
yutian81 <yutian88881@hotmail.com>
2025-11-14 17:12:21 +0800

    Update vpsnpm.sh

--- vpsnpm.sh
+++ vpsnpm.sh
@@ -205,7 +205,7 @@ if [[ -z "$INVOCATION_ID" && -z "$OPENRC_INIT_DIR" ]]; then
 
     echo -e "\n----- 🚀 节点信息 (Base64) -----"
     if [ -f "${SUB_FILE}" ]; then
-        cat "${SUB_FILE}" | base64 | tr -d '\n'
+        cat "${SUB_FILE}"
         echo -e "\n-----------------------------\n"
     else
         echo "❌ 警告：未在预期时间内找到节点信息文件 ${SUB_FILE}。"
